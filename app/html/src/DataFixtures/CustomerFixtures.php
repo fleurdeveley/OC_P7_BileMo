17c85c17538f5efb749c2b3f1edc09eae93ba44f
fleur <fleurdeveley@gmail.com>
2021-08-04 14:27:37 +0200

    modified customer fixtures

--- app/html/src/DataFixtures/CustomerFixtures.php
+++ app/html/src/DataFixtures/CustomerFixtures.php
@@ -6,20 +6,18 @@ use App\Entity\Customer;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
-use Faker\Provider\fr_FR\PhoneNumber;
 
 class CustomerFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create('fr_FR');
-        $faker->addProvider(new PhoneNumber($faker));
 
         for($c = 0; $c < 3; $c++) {
             $customer = new Customer;
 
             $customer->setFullName($faker->name())
-                ->setAddress($faker->address())
+                ->setAddress($faker->streetAddress())
                 ->setPostalCode(mt_rand(10000, 99999))
                 ->setCity($faker->city())
                 ->setEmail("user$c@gmail.com")
