b117fcb5c6e0c4751545487f98699636df3fcc14
fleur <fleurdeveley@gmail.com>
2021-08-06 10:08:13 +0200

    add create user

--- app/html/src/Controller/UserController.php
+++ app/html/src/Controller/UserController.php
@@ -2,15 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
+use App\Repository\CustomerRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 use Knp\Component\Pager\PaginatorInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\Cache\CacheInterface;
-use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Serializer\Exception\NotEncodableValueException;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserController extends AbstractController
 {
@@ -48,7 +55,7 @@ class UserController extends AbstractController
         );
     }
 
-        /**
+    /**
      * @Route("/user/{id}", name="api_user_details", methods={"GET"})
      */
     public function show($id, UserRepository $userRepository)
@@ -57,6 +64,52 @@ class UserController extends AbstractController
             $userRepository->findOneBy(['id' => $id]), 
             JsonResponse::HTTP_OK, 
             [], 
-            ['groups' => 'user:details']);
+            ['groups' => 'user:details']
+        );
+    }
+
+    /**
+     * @Route("/user", name="api_user_create", methods={"POST"})
+     */
+    public function create(
+        Request $request, 
+        SerializerInterface $serializer, 
+        EntityManagerInterface $em,
+        ValidatorInterface $validator,
+        CustomerRepository $customerRepository,
+        UserPasswordHasherInterface $hasher
+    )
+    {
+        $jsonRecu = $request->getContent();
+
+        try {
+            $user = $serializer->deserialize($jsonRecu, User::class, 'json');
+           
+
+            $user->setPassword($hasher->hashPassword($user, 'password'))
+                ->setRoles(['ROLE_USER'])
+                ->setCustomer($customerRepository->findOneBy(['id' => 81]));
+
+            $errors = $validator->validate($user);
+
+            if(count($errors) > 0) {
+                return $this->json($errors, Response::HTTP_BAD_REQUEST);
+            }
+
+            $em->persist($user);
+            $em->flush();
+
+            return $this->json(
+                $user, 
+                Response::HTTP_CREATED, 
+                [],
+                ['groups' => 'user:details']
+            );  
+        } catch(NotEncodableValueException $e) {
+            return $this->json([
+                'status' => Response::HTTP_BAD_REQUEST,
+                'message' => $e->getMessage()
+            ], Response::HTTP_BAD_REQUEST);
+        }
     }
 }
