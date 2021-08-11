99a569926dbad04a9dd47badee11735e3ad2901e
fleur <fleurdeveley@gmail.com>
2021-08-11 20:48:43 +0200

    error management

--- app/html/src/Controller/UserController.php
+++ app/html/src/Controller/UserController.php
@@ -7,6 +7,7 @@ use App\Entity\User;
 use App\Repository\UserRepository;
 use App\Repository\CustomerRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use JMS\Serializer\SerializationContext;
 use JMS\Serializer\Serializer;
 use Symfony\Contracts\Cache\ItemInterface;
@@ -32,13 +33,12 @@ class UserController extends AbstractController
     protected $hasher;
 
     public function __construct(
-        CacheInterface $cache, 
+        CacheInterface $cache,
         UserRepository $userRepository,
         SerializerInterface $serializer,
         ValidatorInterface $validator,
         UserPasswordHasherInterface $hasher
-    ) 
-    {
+    ) {
         $this->cache = $cache;
         $this->userRepository = $userRepository;
         $this->serializer = $serializer;
@@ -50,11 +50,10 @@ class UserController extends AbstractController
      * @Route("/user", name="api_user_list", methods={"GET"})
      */
     public function index(
-        PaginatorInterface $paginator, 
-        Request $request, 
+        PaginatorInterface $paginator,
+        Request $request,
         JMSInterface $serializer
-    ): Response
-    {
+    ): Response {
         $userRepository = $this->userRepository;
 
         $customer_id = $this->getUser()->getCustomer()->getId();
@@ -62,13 +61,16 @@ class UserController extends AbstractController
         $key = 'users_' . $customer_id;
 
         $data = $this->cache->get(
-            $key, 
-            function(ItemInterface $item) 
-            use($userRepository, $customer_id
-        ){
-            $item->expiresAfter(3600);
-            return $userRepository->findBy(['customer' => $customer_id]);
-        });
+            $key,
+            function (ItemInterface $item)
+            use (
+                $userRepository,
+                $customer_id
+            ) {
+                $item->expiresAfter(3600);
+                return $userRepository->findBy(['customer' => $customer_id]);
+            }
+        );
 
         $pagination = $paginator->paginate(
             $data,
@@ -83,13 +85,13 @@ class UserController extends AbstractController
 
         $json = $serializer->serialize(
             $result,
-            'json', 
+            'json',
             SerializationContext::create()->setGroups(array('user:list'))
         );
 
         return new Response(
-            $json, 
-            Response::HTTP_OK, 
+            $json,
+            Response::HTTP_OK,
             array('Content-Type' => 'application/json')
         );
     }
@@ -103,34 +105,35 @@ class UserController extends AbstractController
 
         $userBdd = $this->userRepository->findOneBy(['id' => $id]);
 
-        if($userBdd->getCustomer()->getId() === $customerId) {
-            $json = $serializer->serialize(
-                $userBdd, 
-                'json', 
-                SerializationContext::create()->setGroups(array('user:details'))
-            );
-
-            return new Response(
-                $json, 
-                Response::HTTP_OK, 
-                array('Content-Type' => 'application/json')
-            );
-        } else {
-            return $this->json([
-                'status' => Response::HTTP_UNAUTHORIZED,
-            ], Response::HTTP_UNAUTHORIZED);
+        if($userBdd === null) {
+            throw new Exception('user not found', Response::HTTP_NOT_FOUND);
         }
+
+        if ($userBdd->getCustomer()->getId() !== $customerId) {
+            throw new Exception('unauthorized', Response::HTTP_UNAUTHORIZED);
+        }
+        
+        $json = $serializer->serialize(
+            $userBdd,
+            'json',
+            SerializationContext::create()->setGroups(array('user:details'))
+        );
+
+        return new Response(
+            $json,
+            Response::HTTP_OK,
+            array('Content-Type' => 'application/json')
+        );
     }
 
     /**
      * @Route("/user", name="api_user_create", methods={"POST"})
      */
     public function create(
-        Request $request, 
+        Request $request,
         EntityManagerInterface $em,
         CustomerRepository $customerRepository
-    )
-    {
+    ) {
         try {
             $user = $this->serializer->deserialize($request->getContent(), User::class, 'json');
 
@@ -144,7 +147,7 @@ class UserController extends AbstractController
                 ->setCreatedAt(new DateTime())
                 ->setUpdatedAt(new DateTime());
 
-            if(count($errors) > 0) {
+            if (count($errors) > 0) {
                 return $this->json($errors, Response::HTTP_BAD_REQUEST);
             }
 
@@ -154,12 +157,12 @@ class UserController extends AbstractController
             $this->cache->delete('users_' . $customerId);
 
             return $this->json(
-                $user, 
-                Response::HTTP_CREATED, 
+                $user,
+                Response::HTTP_CREATED,
                 [],
                 ['groups' => 'user:details']
-            );  
-        } catch(NotEncodableValueException $e) {
+            );
+        } catch (NotEncodableValueException $e) {
             return $this->json([
                 'status' => Response::HTTP_BAD_REQUEST,
                 'message' => $e->getMessage()
@@ -179,19 +182,19 @@ class UserController extends AbstractController
 
             $errors = $this->validator->validate($userJson);
 
-            if(count($errors) > 0) {
+            if (count($errors) > 0) {
                 return $this->json($errors, Response::HTTP_BAD_REQUEST);
             }
 
-            if($userJson->getEmail()){
+            if ($userJson->getEmail()) {
                 $user->setEmail($userJson->getEmail());
             }
 
-            if($userJson->getPassword()){
+            if ($userJson->getPassword()) {
                 $user->setPassword($this->hasher->hashPassword($user, $userJson->getPassword()));
             }
 
-            if($userJson->getfullName()){
+            if ($userJson->getfullName()) {
                 $user->setFullName($userJson->getFullName());
             }
 
@@ -205,9 +208,9 @@ class UserController extends AbstractController
 
             return $this->json(
                 [],
-                Response::HTTP_OK 
-            );  
-        } catch(NotEncodableValueException $e) {
+                Response::HTTP_OK
+            );
+        } catch (NotEncodableValueException $e) {
             return $this->json([
                 'status' => Response::HTTP_BAD_REQUEST,
                 'message' => $e->getMessage()
@@ -228,8 +231,8 @@ class UserController extends AbstractController
         $this->cache->delete('users_' . $customerId);
 
         return $this->json(
-            [], 
+            [],
             Response::HTTP_NO_CONTENT
-        );  
+        );
     }
 }
