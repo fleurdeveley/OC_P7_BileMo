c7592239c3a3b852f12083aef93e88a3dd91a689
fleur <fleurdeveley@gmail.com>
2021-08-11 10:21:49 +0200

    install hateoas with jsm serializer

--- app/html/config/bundles.php
+++ app/html/config/bundles.php
@@ -10,4 +10,6 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    Bazinga\Bundle\HateoasBundle\BazingaHateoasBundle::class => ['all' => true],
 ];
